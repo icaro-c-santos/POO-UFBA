b25c96b7cc2c4ca3fff029d873d8df948e499154
ICARO SANTOS <icarosalna@hotmail.com>
2022-05-03 22:33:45 -0300

    MAROCS VINICIUS - TELAS

---
+++ com.sistema-escolar2/src/main/java/Utilitarios/Utilitario.java
@@ -0,0 +1,37 @@
+package Utilitarios;
+
+public class Utilitario {
+	
+	
+	public boolean validarCpf(String cpf) {
+			
+		return false;
+	}
+	
+	public boolean validarEmail(String email) {
+		
+		return false;
+	}
+	
+	public boolean valideNome(String nome) {
+		
+		return false;
+	}
+	
+	public boolean ValideIdade(String data) {
+		
+		return false;
+	}
+	
+	
+	public boolean ValideDate(String data) {
+		
+		return false;
+	}
+	
+	
+	
+	
+	
+	
+}
